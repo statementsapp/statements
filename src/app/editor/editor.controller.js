fb0570e4a7ef233017e49c924562063975f3734d
Statements <statementsapp@gmail.com>
2022-04-23 18:08:51 -0400

    that element

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -2031,7 +2031,7 @@
       $scope.dropItem = function (node, paragraph, proposition, flag, element, event) {
         console.log("Element: ", element ? element : '')
         console.log("Event: ", event ? event : '')
-
+        console.log("That element: ", ('propositionsol' + paragraph.paragraphId))
         if (element && event){
           var relX = event.pageX - $('propositionsol' + paragraph.paragraphId).offset().left;
           var relY = event.pageY - $('propositionsol' + paragraph.paragraphId).offset().top;

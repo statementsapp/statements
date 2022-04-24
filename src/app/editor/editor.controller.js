dfc572342912f4e21615050857eefa05a9a6e2c3
Statements <statementsapp@gmail.com>
2022-04-23 23:13:59 -0400

    comma

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -2046,7 +2046,7 @@
           var relX = event.pageX - $('#propositionsol' + paragraph.paragraphId).offset().left;
           var relY = event.pageY - $('#propositionsol' + paragraph.paragraphId).offset().top;
           console.log(event.pageX ", ", event.pageY)
-          console.log($('#propositionsol' + paragraph.paragraphId).offset().left ", ", $('#propositionsol' + paragraph.paragraphId).offset().top)
+          console.log($('#propositionsol' + paragraph.paragraphId).offset().left, ", ", $('#propositionsol' + paragraph.paragraphId).offset().top)
           console.log(relX, ", ", relY)
           if (relX < 30 && relY < 22){
             console.log("returning left ol")

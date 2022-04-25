809fa719bd94fa92451e2d922f36783fba22487c
Statements <statementsapp@gmail.com>
2022-04-25 16:00:46 -0400

    duplicate fcn

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -2164,19 +2164,20 @@
         }, 20);
       };
 
-      $scope.highlightNode = function (node){
-        for (var i = 0; i < $scope.data[0].nodes.length; i++){
-          for (var j = 0; j < $scope.data[0].nodes[i].paragraphs.length; j++){
-            for (var k = 0; k < $scope.data[0].nodes[i].paragraphs[j].propositions.length; k++){
-              $scope.data[0].nodes[i].paragraphs[j].propositions[k].highlighted = true;
-              for (var m = 0; m < $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks.length; m++){
-                $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[m].highlighted = true;
-              }
-            }
-          }
-        }
+      // $scope.highlightNode = function (node){
+      //   console.log("Highlight node")
+      //   for (var i = 0; i < $scope.data[0].nodes.length; i++){
+      //     for (var j = 0; j < $scope.data[0].nodes[i].paragraphs.length; j++){
+      //       for (var k = 0; k < $scope.data[0].nodes[i].paragraphs[j].propositions.length; k++){
+      //         $scope.data[0].nodes[i].paragraphs[j].propositions[k].highlighted = true;
+      //         for (var m = 0; m < $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks.length; m++){
+      //           $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[m].highlighted = true;
+      //         }
+      //       }
+      //     }
+      //   }
 
-      }
+      // }
 
       $scope.highlightParagraph = function (node, paragraph){
         for (var i = 0; i < $scope.data[0].nodes.length; i++){

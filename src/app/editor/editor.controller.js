ce3bbcaf018dca2da138294ef94d11aae7c0c28c
Statements <statementsapp@gmail.com>
2022-05-01 11:08:04 -0400

    screen

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -623,6 +623,10 @@
       $scope.tempStopEditable = false;
       $scope.minimalStyle = false;
       $scope.diagnostics = false;
+      $scope.screenWidth = window.innerWidth;
+      console.log("Screen width: ", $scope.screenWidth)
+
+
       var hidden = '';
       var visibilityChange = '';
       $scope.theseInputs = [];

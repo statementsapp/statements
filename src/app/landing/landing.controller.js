a6cd40ec4fc5836ea1f91da1004bb1a4521512f0
Statements <statementsapp@gmail.com>
2022-05-02 09:37:34 -0400

    auto dimensions

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -13,7 +13,7 @@
 
     var swiper = new Swiper('.swiper', {
       // Optional parameters
-      direction: 'vertical',
+      direction: 'horizontal',
       loop: true,
 
       // If we need pagination

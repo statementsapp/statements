d4d2e5e07f6cd27b3d77806de9dad83b63a767be
Statements <statementsapp@gmail.com>
2022-05-02 07:07:48 -0400

    require

--- gulp/server.js
+++ gulp/server.js
@@ -6,8 +6,6 @@ const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync');
 
-// where else to put this?
-const Swipe = require('swipejs');
 
 gulp.task('browser-sync', ['nodemon'], function () {
   browserSync.init({
--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -11,6 +11,8 @@
     $scope.screenWidth = window.innerWidth;
     console.log("Screen width: ", $scope.screenWidth)
 
+    const Swipe = require('swipejs');
+
     var element = document.getElementById('mySwipe');
     window.mySwipe = new Swipe(element, {
       startSlide: 0,

f786f3b3562605ebd9861ec111974faa018cbc05
Statements <statementsapp@gmail.com>
2022-05-04 18:54:39 -0400

    no update

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1588,7 +1588,7 @@
                         console.log("If editing copy")
                         $scope.data[0].nodes[i].paragraphs[j].propositions[k].text = $scope.editingCopy;
                         console.log("UPDATED: ", $scope.data[0].nodes[i].paragraphs[j].propositions[k].text)
-                        $scope.updateProposition($scope.data[0].nodes[i], $scope.data[0].nodes[i].paragraphs[j], $scope.data[0].nodes[i].paragraphs[j].propositions[k], 'stale')
+                        // $scope.updateProposition($scope.data[0].nodes[i], $scope.data[0].nodes[i].paragraphs[j], $scope.data[0].nodes[i].paragraphs[j].propositions[k], 'stale')
                         // document.getElementById('proposition' + $scope.data[0].nodes[i].paragraphs[j].propositions[k].id).textContent = $scope.editingCopy;
                       }
                       $scope.editing = '';

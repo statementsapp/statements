20ebf96b87e117ebce44d2ed4cd65f2c497b0b63
Statements <statementsapp@gmail.com>
2022-05-04 21:55:58 -0400

    view value

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1588,7 +1588,7 @@
                         console.log("If editing copy")
                         $scope.data[0].nodes[i].paragraphs[j].propositions[k].text = $scope.editingCopy;
                         console.log("UPDATED: ", $scope.data[0].nodes[i].paragraphs[j].propositions[k])
-                        console.log("SCOPE: ", angular.element(document.getElementById($scope.editing)).scope())
+                        angular.element(document.getElementById($scope.editing)).scope().$setViewValue($scope.editingCopy)
                         // $scope.updateProposition($scope.data[0].nodes[i], $scope.data[0].nodes[i].paragraphs[j], $scope.data[0].nodes[i].paragraphs[j].propositions[k], 'stale')
                         // document.getElementById('proposition' + $scope.data[0].nodes[i].paragraphs[j].propositions[k].id).textContent = $scope.editingCopy;
                       }

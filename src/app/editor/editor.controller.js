e1c69ccea3667285d00f442aeef0a615d113d203
Statements <statements@Statementss-MacBook-Air.local>
2022-11-20 15:52:53 -0500

    console logs

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -339,10 +339,15 @@
       head.appendChild(style);
       $scope.inverted = !$scope.inverted;
 
-      document.getElementById('directlinkinputtext').style.color = 'black';
-      document.getElementById('exporttext').style.color = 'white';
-      document.getElementById('thatdownloadbutton').style.borderColor = 'white';
-      document.getElementById('thatdownloadbutton').style.border = '1px';
+
+      console.log(document.getElementById('directlinkinputtext'));
+      //.style.color = 'black';
+      console.log(document.getElementById('exporttext'));
+      // .style.color = 'white';
+      console.log(document.getElementById('thatdownloadbutton'));
+      //.style.borderColor = 'white';
+      console.log(document.getElementById('thatdownloadbutton'));
+      // .style.border = '1px';
 
       var icon = $scope.inverted ? 'fas fa-sun' : 'fas fa-moon';
       for (var i = 0; i < $scope.menuButtons.length; i++) {

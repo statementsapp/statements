6ebe893e39a6319d14ef9b4561fc5a16fbcf12d4
s <s@ss-MacBook-Air.local>
2022-12-22 11:26:53 -0800

    point seven five

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1067,8 +1067,8 @@
                         var tweetButton = document.getElementById('tweet-button');
                         event.preventDefault();
 
-                        var thisClientX = (parseInt(event.pageX) * 1.33).toString() + 'px';
-                        var thisClientY = (parseInt(event.pageY) * 1.33).toString() + 'px';
+                        var thisClientX = (parseInt(event.pageX) * .75).toString() + 'px';
+                        var thisClientY = (parseInt(event.pageY) * .75).toString() + 'px';
 
                         console.log("this client y: ", thisClientY)
                         console.log("this client x: ", thisClientX)

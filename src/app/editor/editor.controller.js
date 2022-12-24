f8d708ab33852d2b7c8da441c795b6285cd48513
s <s@ss-MacBook-Air.local>
2022-12-24 14:28:43 -0800

    minus

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1072,8 +1072,8 @@
                     var offsetWidth = document.getElementById('wholeprop'+ theId).getBoundingClientRect().left;
                     console.log("That top before: ", document.getElementById('wholeprop'+ theId).getBoundingClientRect().top)
                     console.log("That left before: ", document.getElementById('wholeprop'+ theId).getBoundingClientRect().left)
-                    document.getElementById('tweet-button').style.top = offsetHeight.toString() + 'px';
-                    document.getElementById('tweet-button').style.left = (offsetWidth).toString() + 'px';
+                    document.getElementById('tweet-button').style.top = (offsetHeight-100).toString() + 'px';
+                    document.getElementById('tweet-button').style.left = (offsetWidth-100).toString() + 'px';
                     document.getElementById('tweet-button').classList.add("grow");
                     console.log("That offset height: ", document.getElementById('tweet-button').getBoundingClientRect().top);
                     console.log("That offset width: ", document.getElementById('tweet-button').getBoundingClientRect().left);

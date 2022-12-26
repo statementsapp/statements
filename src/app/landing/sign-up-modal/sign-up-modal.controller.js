7ec4eda1d6fc78bcab13ba26c4125c6c30783bc9
s <s@ss-MacBook-Air.local>
2022-12-26 15:43:42 -0800

    draft sign up

---
+++ src/app/landing/sign-up-modal/sign-up-modal.controller.js
@@ -0,0 +1,70 @@
+(function() {
+  'use strict';
+
+  /** @ngInject */
+  function RegisterModalController($uibModalInstance, $state, ApiService) {
+    var vm = this;
+    vm.apiService = new ApiService();
+
+    vm.errors = {
+      passwordsMatch: null,
+      malformedEmail: null
+    };
+
+    vm.isValid = false;
+    vm.passwordVerify = '';
+
+    vm.user = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    };
+
+    var signUpButton = document.getElementById('signUp')
+    var signInButton = document.getElementById('signIn')
+    var container = document.getElementById('container')
+
+    // signUpButton.addEventListener('click', () => {
+    //   container.classList.add('right-panel-active');
+    // });
+
+    // signInButton.addEventListener('click', () => {
+    //   container.classList.remove('right-panel-active');
+    // });
+
+    vm.register = function(isValid) {
+      if (isValid) {
+        vm.processing = true;
+        vm.apiService.registerWithEmailAndPassword(vm.user.email, vm.user.password)
+          .then(function() {
+            vm.apiService.updateProfile({
+              displayName: vm.user.firstName + ' ' + vm.user.lastName,
+              firstName: vm.user.firstName,
+              lastName: vm.user.lastName,
+              email: vm.user.email
+            }).then(function() {
+              vm.processing = false;
+              $uibModalInstance.dismiss(true);
+            }).catch(function(error) {
+              console.error(error);
+              vm.processing = false;
+            });
+          }, function(error) {
+            console.error(error);
+            vm.processing = false;
+          });
+      }
+    };
+
+    vm.cancel = function() {
+      $uibModalInstance.dismiss();
+    };
+
+  }
+
+  angular.module('statements')
+    .controller('RegisterModalController', RegisterModalController);
+
+})
+();

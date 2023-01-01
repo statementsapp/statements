921e8e97901b9b03f8ded25f3660d1bdb2e0a6d6
s <s@ss-MacBook-Air.local>
2023-01-01 14:51:41 -0800

    cancel

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -221,7 +221,7 @@
     $scope.openLoginModal = function () {
       setTimeout(function() {
         console.log("The modal element: ", document.getElementsByClassName("modal"));
-        document.getElementsByClassName("modal").addEventListener("click", vm.dismiss());
+        document.getElementsByClassName("modal").addEventListener("click", vm.cancel());
       }, 10);
       console.log("The modal element: ", document.getElementsByClassName("modal"));
       $scope.loginModalInstance = $uibModal.open({

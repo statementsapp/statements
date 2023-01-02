5b5478ea119d0bd423666d07aa3934e459b9528c
s <s@ss-MacBook-Air.local>
2023-01-02 15:59:32 -0800

    one

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1082,8 +1082,8 @@
                     };
                     document.getElementById($scope.selectedProposition.id).blur();
 
-                    var offsetHeight = document.getElementById('wholeprop' + theId).getBoundingClientRect().top*0;
-                    var offsetWidth = document.getElementById('wholeprop'+ theId).getBoundingClientRect().left*0;
+                    var offsetHeight = document.getElementById('wholeprop' + theId).getBoundingClientRect().top;
+                    var offsetWidth = document.getElementById('wholeprop'+ theId).getBoundingClientRect().left;
                     console.log("That top before: ", document.getElementById('wholeprop'+ theId).getBoundingClientRect().top)
                     console.log("That left before: ", document.getElementById('wholeprop'+ theId).getBoundingClientRect().left)
                     document.getElementById('tweet-button').style.top = (offsetHeight).toString() + 'px';

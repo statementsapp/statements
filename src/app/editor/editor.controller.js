ecb3328f3e7d1115135042044c97e10b4422de71
s <s@ss-MacBook-Air.local>
2023-01-03 08:50:05 -0800

    amost

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1084,7 +1084,7 @@
                     document.getElementById($scope.selectedProposition.id).blur();
 
                     var offsetHeight = document.getElementById('proposition' + theId).getBoundingClientRect().top-30;
-                    var offsetWidth = document.getElementById('proposition'+ theId).getBoundingClientRect().left-170;
+                    var offsetWidth = document.getElementById('proposition'+ theId).getBoundingClientRect().left-180;
                     console.log("That top before: ", document.getElementById('wholeprop'+ theId).getBoundingClientRect().top)
                     console.log("That left before: ", document.getElementById('wholeprop'+ theId).getBoundingClientRect().left)
                     document.getElementById('tweet-button').style.top = (offsetHeight).toString() + 'px';

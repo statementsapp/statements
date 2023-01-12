e71b07e62651b1007516e4095e50f86e7e6135f2
s <s@ss-MacBook-Air.local>
2023-01-12 00:28:55 -0800

    open book

--- src/app/editor/library-modal/library-modal.controller.js
+++ src/app/editor/library-modal/library-modal.controller.js
@@ -25,6 +25,33 @@
       }
     };
 
+    vm.openBook = function() {
+      vm.processing = true;
+
+      apiService.readBook(vm.bookId).then(function(result) {
+        if (result.data === 'null') {
+          vm.errors.noBookFound = true;
+        } else {
+          vm.errors.noBookFound = false;
+          var bookIds = profileService.getBookIds();
+          bookIds.push(vm.bookId);
+          profileService.setBookIds(bookIds);
+          apiService.updateProfile(profileService.getProfile()).then(function() {
+            vm.processing = false;
+            $uibModalInstance.close(vm.bookId);
+          }).catch(function(error) {
+            console.log(error);
+          });
+        }
+      }).catch(function(error) {
+        vm.errors.misc = error;
+        console.error(error);
+      }).finally(function() {
+        vm.processing = false;
+      });
+
+    };
+
     vm.selectBook = function(bookId) {
       $uibModalInstance.close(bookId);
     };

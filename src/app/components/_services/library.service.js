d5b588dc778c7842d1c6ae0234579f2cc78d2e6b
lq <lq@lqs-MacBook-Air.local>
2023-01-24 23:47:44 -0800

    pushing

--- src/app/components/_services/library.service.js
+++ src/app/components/_services/library.service.js
@@ -24,6 +24,7 @@
         console.log("Key: ", key)
         console.log(key === uids[0])
         if (uids.includes(key)) {
+          console.log("Pushing: ", key)
           books.push({ book: library[key], uid: key });
         }
       }

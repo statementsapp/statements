da7c22b3b633f8ce643cc606efb807824734aec2
lq <lq@lqs-MacBook-Air.lan>
2023-02-24 13:48:51 -0800

    stashing

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -5144,7 +5144,7 @@
                   $scope.hasChatFocusId = id;
                   setTimeout(function () {
                     console.log("Rows: ", document.getElementById('input' + $scope.hasChatFocusId).rows);
-                  console.log("Rows id : ", document.getElementById('input' + id).rows);
+                  console.log("Rows id : ", document.getElementById('input' + id).rows); 
                      focusFactory('input'+ id)
                      console.log("Has chat focus id: ", $scope.hasChatFocusId)
                   }, 20);

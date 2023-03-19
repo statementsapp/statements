e607ac2a34c1f512956e4800d1778cb0b3b418ae
lq <statementsapp@gmail.com>
2023-03-18 20:58:45 -0700

    html

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -3472,6 +3472,8 @@
             text: 'itsown'
           }
           prep.messagesSoFar = [prep.id]
+
+          document.getElementById('top'+paragraph.paragraphId).innerHTML = '';
         } else if (paragraph.bottomAdd){
 
           console.log("3e")

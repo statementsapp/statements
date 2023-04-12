5a0b8aff0014b3da845a1f666de3e3b5ac30d71c
lq <statementsapp@gmail.com>
2023-04-11 21:57:58 -0700

    select it

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -2216,6 +2216,8 @@
       }
 
       $scope.dragProposition = function (node, paragraph, proposition, e) {
+
+        $scope.selectedProposition = proposition;
         if ($scope.cancelListenForDoubleClick){
           $scope.cancelListenForDoubleClick = false;
           return;
@@ -3442,7 +3444,7 @@
 
         // If it's ended with a colon, or a dragged node
         // it's a topic
-        
+
         if ((prep.lastChar === ':' || $scope.draggingNode || $scope.inputs.newSectionTitle) &&
           ($scope.data[0].documentClaimedBy === $scope.userId || !$scope.data[0].documentClaimedBy)) {
           console.log("One")

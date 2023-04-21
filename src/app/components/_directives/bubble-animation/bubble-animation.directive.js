c2f398c93bd2c4a4ec801c728ce3df6a33c414af
lq <statementsapp@gmail.com>
2023-04-21 15:15:35 -0700

    annoying

--- src/app/components/_directives/bubble-animation/bubble-animation.directive.js
+++ src/app/components/_directives/bubble-animation/bubble-animation.directive.js
@@ -4,15 +4,7 @@
   /** @ngInject */
   function bubbleAnimation($timeout) {
     return {
-    		
-    		    // link: function(scope, element, attrs) {
-    		    //   // Apply the 'new-message' class only to the new messages added after the initial load
-    		    //   if (scope.$last) {
-    		    //     $timeout(function() {
-    		    //       element.addClass('new-message');
-    		    //     }, 0);
-    		    //   }
-    		    // }
+
     		  
     };
   }

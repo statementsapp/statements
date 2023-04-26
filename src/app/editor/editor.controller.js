475495ad5df5c80a0138b0233e5a5432c9453b2c
lq <statementsapp@gmail.com>
2023-04-25 23:04:29 -0700

    console log

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -4497,6 +4497,7 @@
 
               for (var i = 0; i < $scope.data[0].nodes[apply.nodeIndex].paragraphs[apply.afterParagraphIndex].propositions.length; i++){
                 for (var j = 0; j < $scope.data[0].nodes[apply.nodeIndex].paragraphs[apply.afterParagraphIndex].propositions[i].remarks.length; j++){
+                  console.log("3B considering: ", $scope.data[0].nodes[apply.nodeIndex].paragraphs[apply.afterParagraphIndex].propositions[i].remarks[j])
                   if (payload.rejoins === $scope.data[0].nodes[apply.nodeIndex].paragraphs[apply.afterParagraphIndex].propositions[i].remarks[j].id){
                     $scope.data[0].nodes[apply.nodeIndex].paragraphs[apply.afterParagraphIndex].propositions[i].remarks[j].rejoined = true;
                     break;

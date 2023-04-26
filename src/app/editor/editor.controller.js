5881a44ad795414a73c48c8ea399a54759b3ab74
lq <statementsapp@gmail.com>
2023-04-26 00:10:38 -0700

    comment out code

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -4383,22 +4383,22 @@
                 }
               )
 
-              for (var j = 0; j < $scope.data[0].nodes[i].paragraphs.length; j++){
+              // for (var j = 0; j < $scope.data[0].nodes[i].paragraphs.length; j++){
 
-                for (var k = 0; k < $scope.data[0].nodes[i].paragraphs[j].propositions.length; k++){
-                  for (var l = 0; l < $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks.length; l++)
-                  if ($scope.selectedProposition.id){
-                    if ($scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[l].id === $scope.selectedProposition.id &&
-                      !$scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[l].hiddenForAll &&
-                      ($scope.selectedProposition.textSide || !$scope.selectedProposition.dialogueSide)){
-                      apply.reselectTarget = $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[l].id;
-                      break;
-                    }
-                  }
+              //   for (var k = 0; k < $scope.data[0].nodes[i].paragraphs[j].propositions.length; k++){
+              //     for (var l = 0; l < $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks.length; l++)
+              //     if ($scope.selectedProposition.id){
+              //       if ($scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[l].id === $scope.selectedProposition.id &&
+              //         !$scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[l].hiddenForAll &&
+              //         ($scope.selectedProposition.textSide || !$scope.selectedProposition.dialogueSide)){
+              //         apply.reselectTarget = $scope.data[0].nodes[i].paragraphs[j].propositions[k].remarks[l].id;
+              //         break;
+              //       }
+              //     }
                     
 
-                }
-              }
+              //   }
+              // }
 
 
               if (payload.author === $scope.userId && !payload.dialogueSide && !$scope.selectedProposition.id){
@@ -4406,7 +4406,7 @@
                 setTimeout(function () {
                   document.getElementById('proposition' + payload.id).click();
                 }, 20);
-              } else if (apply.reselectNodeTarget){
+              } else if (apply.reselectTarget && $scope.cowsComeHome){
                 console.log("2B Collision click")
                 setTimeout(function () {
                   document.getElementById('remark' + apply.reselectTarget).click();

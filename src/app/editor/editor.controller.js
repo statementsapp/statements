0d9f706ebfb26df2fea11357026eb5ea32f3c515
lq <statementsapp@gmail.com>
2023-05-02 14:56:37 -0700

    not of

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -5100,7 +5100,7 @@
               
               console.log("All negations")
 
-              if ($scope.data[0].dialogue[$scope.data[0].dialogue.length-1].of.id === payload.of.id){
+              if ($scope.data[0].dialogue[$scope.data[0].dialogue.length-1].id === payload.of.id){
                 // if it's responding to the last one, just push it
                 $scope.data[0].dialogue.push(
                 {

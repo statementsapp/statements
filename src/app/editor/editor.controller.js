ba79611009dd81ed21b6ac90ab20356cb342009f
lq <statementsapp@gmail.com>
2023-05-21 11:45:08 -0700

    code

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -6159,7 +6159,7 @@
 
                           if (
                             $scope.preDefinedPoints[theOn].text.slice(0, 6) === $scope.data[0].nodes[h].paragraphs[i].propositions[j].remarks[k].text.slice(0,6)){
-                            var thisHereId = $scope.data[0].nodes[h].paragraphs[i].propositions[j].id;
+                            var thisHereId = $scope.data[0].nodes[h].paragraphs[i].propositions[j].remarks[k].id;
                             var thisH = angular.copy(h)
                             var thisI = angular.copy(i)
                             var thisJ = angular.copy(j)

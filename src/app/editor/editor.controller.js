526dddd5180021b962dccde527a3065435941316
lq <statementsapp@gmail.com>
2023-05-26 17:59:11 -0700

    timeout

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -6210,10 +6210,10 @@
                             document.getElementById($scope.data[0].nodes[thisH].paragraphs[thisI].paragraphId).click();
                           }, 20);
                           // $scope.$apply(function () {
-                          //   setTimeout(function () {
-                          //     populateElementWithText($scope.preDefinedPoints[index].text, $scope.data[0].nodes[thisH].paragraphs[thisI].paragraphId)
-                          //     console.log("Got an id")
-                          //   }, 20);
+                            setTimeout(function () {
+                              populateElementWithText($scope.preDefinedPoints[index].text, $scope.data[0].nodes[thisH].paragraphs[thisI].paragraphId)
+                              console.log("Got an id")
+                            }, 20);
                           // });
                           
                         } else if (theStep.its === 'left' && !hasAK){

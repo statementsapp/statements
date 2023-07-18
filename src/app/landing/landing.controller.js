5f2ea06afdebb29d707e31113e1960257c35760b
lq <statementsapp@gmail.com>
2023-07-18 02:18:04 -0400

    try

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -14,24 +14,25 @@
 
 
     $scope.saveEmail = function () {
-          var email = $scope.userEmail; // Get the email entered by the user
-          if (email) {
-            var database = firebase.database();
-            var emailsRef = database.ref('emails');
-            var newEmailRef = emailsRef.push();
-            newEmailRef.set({
-              email: email
-            }, function (error) {
-              if (error) {
-                console.error('Error saving email:', error);
-              } else {
-                console.log('Email saved successfully!');
-              }
-            });
+      var email = $scope.userEmail; // Get the email entered by the user
+      if (email && email.trim() !== '') {
+        var database = firebase.database();
+        var emailsRef = database.ref('emails');
+        var newEmailRef = emailsRef.push();
+        newEmailRef.set({
+          email: email
+        }, function (error) {
+          if (error) {
+            console.error('Error saving email:', error);
           } else {
-            console.log('Please enter an email address.');
+            console.log('Email saved successfully!');
           }
+        });
+      } else {
+        console.log('Please enter a valid email address.');
+      }
     };
+    
 
     
     $scope.videoOne = document.getElementById('exampleAuthor')

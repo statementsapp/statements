13e9e9df6b5e3f5707acea0b71b3e0a13064e17f
lq <statementsapp@gmail.com>
2023-08-22 16:12:53 -0700

    update

--- src/app/components/_directives/check-cursor-visibility/check-cursor-visibility-directive.js
+++ src/app/components/_directives/check-cursor-visibility/check-cursor-visibility-directive.js
@@ -8,29 +8,33 @@
       link: function(scope, elem, attrs) {
         let parentContainer = elem.parent(); // Assuming direct parent is the scroll container
 
-        elem.on('keyup', function() {
-            $timeout(function() {
-                let sel = window.getSelection();
-                if (!sel.rangeCount) return;
+              elem.on('keyup', function() {
+                $timeout(function() {
+                  let sel = window.getSelection();
+                  if (!sel.rangeCount) return;
 
-                let range = sel.getRangeAt(0).cloneRange();
-                range.collapse(true);
-                let dummy = document.createElement('span');
-                range.insertNode(dummy);
-                let rect = dummy.getBoundingClientRect();
-                let parentRect = parentContainer[0].getBoundingClientRect();
-                dummy.parentNode.removeChild(dummy);
+                  let range = sel.getRangeAt(0).cloneRange();
+                  range.collapse(true);
+                  let dummy = document.createElement('span');
+                  range.insertNode(dummy);
+                  
+                  let dummyRect = dummy.getBoundingClientRect();
+                  let parentRect = parentContainer[0].getBoundingClientRect();
 
-                if (rect.top < parentRect.top) {
-                // Cursor is above the viewable area
-                parentContainer[0].scrollTop -= (parentRect.top - rect.top);
-                } else if (rect.bottom > parentRect.bottom) {
-                // Cursor is below the viewable area
-                    console.log('B E L O W')
-                parentContainer[0].scrollTop += (rect.bottom - parentRect.bottom);
-                }
-            });
-        });
+                  if (dummyRect.bottom > parentRect.bottom) {
+                    // Cursor is below the viewable area
+                    let scrollNeeded = dummyRect.bottom - parentRect.bottom;
+                    parentContainer[0].scrollTop += scrollNeeded;
+                  } else if (dummyRect.top < parentRect.top) {
+                    console.log("BELOW VISIBLE ELSE")
+                    // Cursor is above the viewable area
+                    parentContainer[0].scrollTop -= parentRect.top - dummyRect.top;
+                  }
+                  
+                  // Remove the dummy span
+                  dummy.parentNode.removeChild(dummy);
+                });
+              });
       }
     };
   }

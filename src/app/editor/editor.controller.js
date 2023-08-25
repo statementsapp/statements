d28cb6b9cce99f7d0af36f662cd919caf3c1403e
lq <statementsapp@gmail.com>
2023-08-24 19:14:34 -0700

    console logs

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -4509,6 +4509,8 @@
                 for (var j = 0; j < $scope.data[0].nodes[i].paragraphs.length; j++){
 
                   for (var k = 0; k < $scope.data[0].nodes[i].paragraphs[j].propositions.length; k++){
+                    console.log("That id: ", angular.copy(payload.of.id))
+                    console.log("That of id: ", angular.copy(($scope.data[0].nodes[i].paragraphs[j].propositions[k].id)
                     if ($scope.data[0].nodes[i].paragraphs[j].propositions[k].id === payload.of.id &&
                       !$scope.data[0].nodes[i].paragraphs[j].propositions[k].hiddenForAll){
                       console.log("GOT YA!")

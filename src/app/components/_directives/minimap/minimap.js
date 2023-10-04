fda6e0d202db03592148e1e66ad77307d2f7b663
lq <statementsapp@gmail.com>
2023-10-03 19:42:17 -0700

    try

--- src/app/components/_directives/minimap/minimap.js
+++ src/app/components/_directives/minimap/minimap.js
@@ -5,53 +5,32 @@
         .module('statements')
         .directive('minimap', minimap);
 
-    minimap.$inject = ['$compile', '$timeout'];
+    minimap.$inject = ['$timeout'];
 
-    function minimap($compile, $timeout) {
+    function minimap($timeout) {
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
-                const scaleFactor = 0.2;
-                let viewport = null;
-
                 $timeout(function() {
                     const targetElement = angular.element(document.querySelector(attrs.targetSelector));
+
+                    // Get outerHTML of the target element
                     const contentHTML = targetElement[0].outerHTML;
 
-                    element.empty();
-
+                    // Create a minimap content container
                     const minimapContent = angular.element('<div class="minimap-content"></div>');
+
+                    // Set its content to be the outerHTML of the target element
                     minimapContent.html(contentHTML);
+                    
+                    // Scale down the content
                     minimapContent.css({
-                        'transform': `scale(${scaleFactor})`,
+                        'transform': 'scale(0.2)',  // scale to 20% of the original size
                         'transform-origin': 'top left'
                     });
-
+                    
+                    // Append this to the main minimap container
                     element.addClass('minimap-container').append(minimapContent);
-                    $compile(minimapContent.contents())(scope);
-
-                    if (!viewport) {
-                        viewport = angular.element('<div class="minimap-viewport"></div>');
-                        element.append(viewport);
-                    }
-
-                    function updateViewport() {
-                        // Calculate the size and position of the viewport based on the scroll position
-                        const viewHeight = window.innerHeight * scaleFactor;
-                        const viewWidth = window.innerWidth * scaleFactor;
-                        const scrollTop = targetElement.scrollTop() * scaleFactor;
-                        const scrollLeft = targetElement.scrollLeft() * scaleFactor;
-
-                        viewport.css({
-                            'height': `${viewHeight}px`,
-                            'width': `${viewWidth}px`,
-                            'top': `${scrollTop}px`,
-                            'left': `${scrollLeft}px`
-                        });
-                    }
-
-                    targetElement.on('scroll', updateViewport);
-                    updateViewport();
                 });
             }
         };

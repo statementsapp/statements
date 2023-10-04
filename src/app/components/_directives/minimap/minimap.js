f8e08b594e30f8c451b9b7e47007bdd8a0db78f1
lq <statementsapp@gmail.com>
2023-10-03 19:02:41 -0700

    updated directive

--- src/app/components/_directives/minimap/minimap.js
+++ src/app/components/_directives/minimap/minimap.js
@@ -5,36 +5,35 @@
         .module('statements')
         .directive('minimap', minimap);
 
-    minimap.$inject = ['$compile'];
+    minimap.$inject = ['$compile', '$timeout'];
 
-    function minimap($compile) {
+    function minimap($compile, $timeout) {
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
-                const targetElement = angular.element(document.querySelector(attrs.targetSelector));
-                const clone = targetElement.clone();
+                $timeout(function() {
+                    const targetElement = angular.element(document.querySelector(attrs.targetSelector));
+                    const clone = targetElement.clone();
 
-                // Try to capture the content of stmnts-editor-text-renderer and replace it in the cloned version
-                const originalStmntsElements = targetElement[0].querySelectorAll('stmnts-editor-text-renderer');
-                const clonedStmntsElements = clone[0].querySelectorAll('stmnts-editor-text-renderer');
-                
-                for (let i = 0; i < originalStmntsElements.length; i++) {
-                    const originalContent = angular.element(originalStmntsElements[i]).contents();
-                    const clonedContent = originalContent.clone();
-                    
-                    // Empty the cloned stmnts-editor-text-renderer and append the cloned content
-                    angular.element(clonedStmntsElements[i]).empty().append(clonedContent);
-                }
+                    // Handle transcluded content
+                    const originalStmntsElements = targetElement[0].querySelectorAll('stmnts-editor-text-renderer');
+                    const clonedStmntsElements = clone[0].querySelectorAll('stmnts-editor-text-renderer');
 
-                // Wrap the clone with the minimap content div
-                const minimapContent = angular.element('<div class="minimap-content"></div>').append(clone);
-                element.addClass('minimap-container').append(minimapContent);
+                    for (let i = 0; i < originalStmntsElements.length; i++) {
+                        const originalContent = angular.element(originalStmntsElements[i]).contents();
+                        const clonedContent = originalContent.clone();
+                        angular.element(clonedStmntsElements[i]).empty().append(clonedContent);
+                    }
 
-                // Compile the cloned content so that AngularJS bindings are kept intact
-                $compile(minimapContent)(scope);
+                    // Wrap the clone with the minimap content div
+                    const minimapContent = angular.element('<div class="minimap-content"></div>').append(clone);
+                    element.addClass('minimap-container').append(minimapContent);
+
+                    // Compile the cloned content to ensure AngularJS bindings are processed
+                    $compile(minimapContent)(scope);
+                });
             }
         };
     }
 
 })();
-

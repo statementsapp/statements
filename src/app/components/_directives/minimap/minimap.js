616fee197a1d4f8abfc5ea1f7171a9ef50ed3159
lq <statementsapp@gmail.com>
2023-10-03 18:56:05 -0700

    minimap

--- src/app/components/_directives/minimap/minimap.js
+++ src/app/components/_directives/minimap/minimap.js
@@ -14,9 +14,20 @@
                 const targetElement = angular.element(document.querySelector(attrs.targetSelector));
                 const clone = targetElement.clone();
 
+                // Try to capture the content of stmnts-editor-text-renderer and replace it in the cloned version
+                const originalStmntsElements = targetElement[0].querySelectorAll('stmnts-editor-text-renderer');
+                const clonedStmntsElements = clone[0].querySelectorAll('stmnts-editor-text-renderer');
+                
+                for (let i = 0; i < originalStmntsElements.length; i++) {
+                    const originalContent = angular.element(originalStmntsElements[i]).contents();
+                    const clonedContent = originalContent.clone();
+                    
+                    // Empty the cloned stmnts-editor-text-renderer and append the cloned content
+                    angular.element(clonedStmntsElements[i]).empty().append(clonedContent);
+                }
+
                 // Wrap the clone with the minimap content div
                 const minimapContent = angular.element('<div class="minimap-content"></div>').append(clone);
-                
                 element.addClass('minimap-container').append(minimapContent);
 
                 // Compile the cloned content so that AngularJS bindings are kept intact
@@ -26,3 +37,4 @@
     }
 
 })();
+

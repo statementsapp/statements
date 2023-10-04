451c34cc77592ef06e51679e57d705d907daa909
lq <statementsapp@gmail.com>
2023-10-03 19:07:34 -0700

    directive

--- src/app/components/_directives/minimap/minimap.js
+++ src/app/components/_directives/minimap/minimap.js
@@ -13,24 +13,24 @@
             link: function(scope, element, attrs) {
                 $timeout(function() {
                     const targetElement = angular.element(document.querySelector(attrs.targetSelector));
-                    const clone = targetElement.clone();
 
-                    // Handle transcluded content
-                    const originalStmntsElements = targetElement[0].querySelectorAll('stmnts-editor-text-renderer');
-                    const clonedStmntsElements = clone[0].querySelectorAll('stmnts-editor-text-renderer');
+                    // Get outerHTML of the target element
+                    const contentHTML = targetElement[0].outerHTML;
 
-                    for (let i = 0; i < originalStmntsElements.length; i++) {
-                        const originalContent = angular.element(originalStmntsElements[i]).contents();
-                        const clonedContent = originalContent.clone();
-                        angular.element(clonedStmntsElements[i]).empty().append(clonedContent);
-                    }
+                    // Clear existing content in minimap
+                    element.empty();
+                    
+                    // Create a minimap content container
+                    const minimapContent = angular.element('<div class="minimap-content"></div>');
 
-                    // Wrap the clone with the minimap content div
-                    const minimapContent = angular.element('<div class="minimap-content"></div>').append(clone);
+                    // Set its content to be the outerHTML of the target element
+                    minimapContent.html(contentHTML);
+                    
+                    // Append this to the main minimap container
                     element.addClass('minimap-container').append(minimapContent);
 
-                    // Compile the cloned content to ensure AngularJS bindings are processed
-                    $compile(minimapContent)(scope);
+                    // Now compile this content with the scope to process Angular directives, bindings, etc.
+                    $compile(minimapContent.contents())(scope);
                 });
             }
         };

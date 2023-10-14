a8911765897f56851791a9654bb447aaef6537e9
lq <statementsapp@gmail.com>
2023-10-14 17:18:48 -0600

    script

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -5797,7 +5797,7 @@
           console.log("This point index: ", thisPointIndex)
           console.log("Script: ", script)
 
-          return script.sequence[thisPointIndex].some(point => point.index === thisPointIndex);
+          return script.[thisPointIndex].some(point => point.index === thisPointIndex);
         }
 
 

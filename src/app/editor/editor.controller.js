e41ab42189685071ec7a3cfa8802b338fbd1ec46
lq <statementsapp@gmail.com>
2023-10-18 10:43:47 -0400

    text

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -10441,7 +10441,7 @@
                     // console.log("Topic: ", $scope.data[0].nodes[h].topic)
                     // console.log("The Node Topic: ", $scope.preDefinedPoints[theOn].text)
 
-                    if ($scope.data[0].nodes[h].topic.slice(0, 11) === script.sequence[theOn].topic.slice(0, 11)){
+                    if ($scope.data[0].nodes[h].topic.slice(0, 11) === script.sequence[theOn].text.slice(0, 11)){
                       console.log("Found")
                       var thisHereId = $scope.data[0].nodes[h].paragraphs[0].propositions[0].id;
                       console.log("Got a this here id: ", thisHereId)

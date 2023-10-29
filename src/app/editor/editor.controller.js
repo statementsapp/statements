e9319730fb1226f8cf5650acd524961e283a8926
lq <statementsapp@gmail.com>
2023-10-29 13:16:57 -0400

    further down the line

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -4202,14 +4202,7 @@
 
 
 
-        const comparatorText = "Today, there are tens of thousands of unique digital assets on the market, available on hundreds of exchanges worldwide, representing a total market capitalization of approximately $1.2 trillion.";
-
-        if (comparatorText.substring(0, 8) === prep.adjustedText.substring(0, 8)) {
-            // Add your code here for the matching case
-          return;
-        } else {
-            console.log("Carry on");
-        }
+        
 
 
 
@@ -4292,6 +4285,15 @@
 
         $scope.statementHighlightIs = '';
 
+
+        const comparatorText = "Today, there are tens of thousands of unique digital assets on the market, available on hundreds of exchanges worldwide, representing a total market capitalization of approximately $1.2 trillion.";
+
+        if (comparatorText.substring(0, 8) === prep.adjustedText.substring(0, 8)) {
+            // Add your code here for the matching case
+          return;
+        } else {
+            console.log("Carry on");
+        }
         //      CLEARS THINGS AND EMITS THE PAYLOAD
         chatSocket.emit('proposition', $scope.userId, prep.payload, $scope.bookId);
 

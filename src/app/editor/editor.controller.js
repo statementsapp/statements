a7179fcb70ccacd65157e82f75e35a19698a88df
ok <ok@oks-MacBook-Air.local>
2024-01-20 19:15:44 +0700

    resistance

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -4223,7 +4223,7 @@
         }
 
         prep.payload = {
-          authorNumber: (authorNumber) ? authorNumber : event ? (event.authorNumber) ? event.authorNumber : (event.authorNumber  || event.authorNumber !== undefined) ? event.authorNumber : authorNumber !== undefined ? authorNumber : undefined,
+          authorNumber: (authorNumber) ? authorNumber : (event.authorNumber) ? event.authorNumber : (event.authorNumber !== undefined) ? event.authorNumber : undefined,
           author: prep.author ? prep.author : $scope.userId,
           text: prep.adjustedText,
           dialogueText: angular.copy(prep.adjustedText),

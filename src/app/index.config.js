af2728ad6eba33c2f0c3185526c72041f1e28cd7
ok <ok@oks-MacBook-Air.local>
2024-01-30 13:20:41 +0700

    undo config change

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -14,17 +14,17 @@
     toastrConfig.positionClass = 'toast-bottom-right';
     toastrConfig.preventDuplicates = true;
     toastrConfig.progressBar = true;
-    // var firebaseConfig = {
-    //   apiKey: "AIzaSyDfrkcBavme0JXpZ1qxP070RexWU7Ct1l4",
-    //   authDomain: "statements-275d0.firebaseapp.com",
-    //   databaseURL: "https://statements-275d0-default-rtdb.firebaseio.com",
-    //   projectId: "statements-275d0",
-    //   storageBucket: "statements-275d0.appspot.com",
-    //   messagingSenderId: "764215061762",
-    //   appId: "1:764215061762:web:997def1495aa0e79af261b",
-    //   measurementId: "G-XZ5HWVV5QX"
-    // };
-    // firebase.initializeApp(firebaseConfig);
+    var firebaseConfig = {
+      apiKey: "AIzaSyDfrkcBavme0JXpZ1qxP070RexWU7Ct1l4",
+      authDomain: "statements-275d0.firebaseapp.com",
+      databaseURL: "https://statements-275d0-default-rtdb.firebaseio.com",
+      projectId: "statements-275d0",
+      storageBucket: "statements-275d0.appspot.com",
+      messagingSenderId: "764215061762",
+      appId: "1:764215061762:web:997def1495aa0e79af261b",
+      measurementId: "G-XZ5HWVV5QX"
+    };
+    firebase.initializeApp(firebaseConfig);
   }
 
   angular.module('statements').config(config);

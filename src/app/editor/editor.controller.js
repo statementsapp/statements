660326fdb233a7ff92f1e8feef99bf7143dc039b
ok <ok@oks-MacBook-Air.local>
2024-07-25 15:30:23 +0700

    dialogue side

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -4132,7 +4132,7 @@
           prep.messagesSoFar = [prep.id]
 
         } else if (
-                  ($scope.selectedProposition.type === 'assertion' && (node && ($scope.userId === node.sectionClaimedBy || $scope.selectedProposition.dialogueSide || (node.isTitle && $scope.data[0].documentClaimedBy === $scope.userId)))) && 
+                  ($scope.selectedProposition.type === 'assertion' && (node && ($scope.userId === node.sectionClaimedBy  || (node.isTitle && $scope.data[0].documentClaimedBy === $scope.userId))) || $scope.selectedProposition.dialogueSide) && 
                   (!$scope.draggingParagraph || proposition.type !== 'blank') && 
                   ($scope.draggedProposition.id || $scope.selectedProposition.type !== 'blank') || 
                   automatedCode === '3G'

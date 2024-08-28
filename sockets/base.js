425ae8791052bd81d254f93242f828efc8b3f27b
ok <ok@oks-MacBook-Air.local>
2024-08-28 18:47:17 +0700

    no timeout

--- sockets/base.js
+++ sockets/base.js
@@ -1,77 +1,3 @@
-// 'use strict';
-
-// var ioRoutes = function(io) {
-//   var userCount = 0;
-
-//   io.on('connection', function(socket) {
-//     socket.broadcast.emit('user connected');
-
-//     io.sockets.emit('A user just connected');
-
-//     userCount++;
-
-//     socket.on('message', function(from, msg) {
-//       io.sockets.emit('broadcast', {
-//         payload: msg,
-//         source: from
-//       });
-//     });
-
-//     socket.on('connect', () => {
-//         console.log('Connected to the server');
-//       });
-
-
-
-//       socket.on('reconnect', () => {
-//         console.log('Reconnected to the server');
-//       });
-
-//       socket.on('reconnect_attempt', () => {
-//         console.log('Attempting to reconnect');
-//       });
-
-//       socket.on('reconnect_failed', () => {
-//         console.log('Reconnection failed');
-//       });
-
-//     socket.on('disconnect', function() {
-//       console.log('Socket disconnected');
-//     });
-    
-//     socket.on('proposition', function(from, obj) { //for first emission
-//       io.sockets.emit('broadcastProposition', obj);
-//     });
-
-//     socket.on('deletion', function(from, obj) { //for first emission
-//       io.sockets.emit('broadcastDeletion', obj);
-//     });
-
-//     socket.on('update', function(from, obj) { //for first emission
-//       io.sockets.emit('broadcastUpdate', obj);
-//     });    
-
-//     socket.on('nodeUpdate', function(from, obj) { //for first emission
-//       io.sockets.emit('broadcastNodeUpdate', obj);
-//     });
-
-//   });
-
-//   // code for indefinite reconnect attempts
-//   io.connect().on("reconnecting", function(delay, attempt) {
-//     console.log("Reconnecting")
-//     // if (attempt === max_socket_reconnects) {
-//     //   setTimeout(function(){ socket.socket.reconnect(); }, 5000);
-//     //   return console.log("Failed to reconnect. Lets try that again in 5 seconds.");
-//     // }
-//   });
-
-
-// };
-
-// module.exports = ioRoutes;
-
-
 'use strict';
 var ioRoutes = function(io) {
   var userCount = 0;
@@ -81,13 +7,23 @@ var ioRoutes = function(io) {
   io.reconnectionAttempts(Infinity);
   io.reconnectionDelay(1000);
   io.reconnectionDelayMax(5000);
-  io.timeout(10000);
+  io.timeout(0); // Set timeout to 0 to disable it
 
   io.on('connection', function(socket) {
     console.log('A user connected');
     socket.broadcast.emit('user connected');
     io.sockets.emit('A user just connected');
     userCount++;
+
+    // Implement heartbeat mechanism
+    const heartbeatInterval = setInterval(() => {
+      socket.emit('heartbeat');
+    }, 25000); // Send heartbeat every 25 seconds
+
+    socket.on('heartbeat', () => {
+      // Respond to heartbeat
+      socket.emit('heartbeat-response');
+    });
 
     socket.on('message', function(from, msg) {
       io.sockets.emit('broadcast', {
@@ -118,8 +54,8 @@ var ioRoutes = function(io) {
 
     socket.on('disconnect', (reason) => {
       console.log('Socket disconnected. Reason: ' + reason);
+      clearInterval(heartbeatInterval); // Clear the heartbeat interval on disconnect
       if (reason === 'io server disconnect') {
-        // Reconnect manually if the server disconnected
         socket.connect();
       }
     });

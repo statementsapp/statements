67da55c4ec9701652b7bc0945c9bd05d826b0ecb
ok <ok@oks-MacBook-Air.local>
2024-09-05 20:58:08 +0700

    test other

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -10061,7 +10061,7 @@
               ]
             }
             // $scope.allTheScripts = [$scope.coinbase, $scope.coinbase2, $scope.coinbase3]
-            $scope.allTheScripts = [$scope.safety]
+            $scope.allTheScripts = [$scope.coinbase]
             // $scope.allTheScripts = [$scope.gamesone]
 
             
@@ -11147,7 +11147,7 @@
 
             // $scope.simulateUser(0, $scope.coinbase3);
             // $scope.simulateUser(0, $scope.coinbase2, 1000);
-            $scope.simulateUser(0, $scope.safety);
+            $scope.simulateUser(0, $scope.coinbase);
           
           
         }, 5000);

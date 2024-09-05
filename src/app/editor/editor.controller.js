b00d57dc4896ea130d3a9d85f14523b50648a359
ok <ok@oks-MacBook-Air.local>
2024-09-05 14:29:16 +0700

    just one

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -9818,8 +9818,8 @@
             
             // $scope.simulateUser(0, $scope.gamesone, 3000);
 
-            $scope.simulateUser(0, $scope.coinbase3);
-            $scope.simulateUser(0, $scope.coinbase2, 1000);
+            // $scope.simulateUser(0, $scope.coinbase3);
+            // $scope.simulateUser(0, $scope.coinbase2, 1000);
             
             $scope.simulateUser(0, $scope.coinbase1);
           

9fd3a01634733ebbb62b7e0af6f5b97461adbaee
ok <ok@oks-MacBook-Air.local>
2024-09-06 00:38:52 +0700

    none

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -9538,7 +9538,7 @@
 
                   which: 'item',
                   on: 7,
-                  its: 'bottom',
+                  its: 'self',
                   typeTime: 3000,
                   noClick: false, 
                   action: 'proposition'
